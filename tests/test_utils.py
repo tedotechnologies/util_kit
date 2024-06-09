ee1592fb5e799d1da3af63fa3beaf286da40e53e
mrYush <yuriy.simonov@tedotechnologies.com>
2024-06-09 16:54:56 +0300

    adding tests

---
+++ tests/test_utils.py
@@ -0,0 +1,68 @@
+# tests/test_utils.py
+import unittest
+import tempfile
+import logging
+import yaml
+from pathlib import Path
+from utilkit import get_config, get_logger, get_kwargs
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_get_config(self):
+        # Создание временного YAML файла
+        with tempfile.NamedTemporaryFile('w', delete=False,
+                                         suffix='.yaml') as temp_file:
+            yaml_content = {
+                'key1': 'value1',
+                'key2': {
+                    'subkey1': 'subvalue1'
+                }
+            }
+            yaml.dump(yaml_content, temp_file)
+            temp_file_path = Path(temp_file.name)
+
+        # Тестирование функции get_config
+        config = get_config(temp_file_path)
+        self.assertEqual(config['key1'], 'value1')
+        self.assertEqual(config['key2']['subkey1'], 'subvalue1')
+
+        # Удаление временного файла
+        temp_file_path.unlink()
+
+    def test_get_logger(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_dir_path = Path(temp_dir)
+            logger = get_logger(logger_name='test_logger', path=temp_dir_path,
+                                level=logging.DEBUG)
+
+            self.assertEqual(logger.name, 'test_logger')
+            self.assertEqual(logger.level, logging.DEBUG)
+
+            # Проверка создания лог файла
+            log_file = temp_dir_path / 'test_logger.log'
+            self.assertTrue(log_file.exists())
+
+            # Проверка записи логов
+            logger.debug('This is a debug message')
+            with open(log_file, 'r') as f:
+                log_content = f.read()
+            self.assertIn('This is a debug message', log_content)
+
+    def test_get_kwargs(self):
+        default_config_path = Path('/path/to/default/config.yaml')
+        parser = get_kwargs(default_config_path)
+
+        args = parser.parse_args([])
+        self.assertEqual(args.config_path, default_config_path)
+        self.assertEqual(args.logger_level, 20)
+
+        args = parser.parse_args(
+            ['-p', '/custom/path/config.yaml', '-l', '10']
+        )
+        self.assertEqual(args.config_path, Path('/custom/path/config.yaml'))
+        self.assertEqual(args.logger_level, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utilkit/utils.py
+++ utilkit/utils.py
@@ -1,6 +1,7 @@
 # utilkit/utils.py
 import argparse
 import logging
+import sys
 from pathlib import Path
 from typing import Dict, Any, Optional
 import yaml
@@ -13,20 +14,51 @@ def get_config(path: Path) -> Dict[str, Any]:
     return exp_config
 
 
-def get_logger(logger_name: Optional[str] = None, path: Optional[Path] = None,
-               level: int = logging.DEBUG) -> logging.Logger:
+
+def get_logger(
+        logger_name: str | None = None,
+        path: Path | None = None,
+        level: int = logging.DEBUG,
+        add_stdout: bool = False) -> logging.Logger:
     """
-    Logger initializing
+    Get logger with file handler
+    Parameters
+    ----------
+    logger_name: str|None
+        Name of logger
+    path: Path|None
+        Path to log file
+    level: int
+        Level of logger
+    add_stdout: bool
+        if true logger will print to stdout too
     """
-    logger_name = 'logs' if logger_name is None else logger_name
-    path_to_logs = Path('logs') if path is None else Path(path)
+    logger_name = "logs" if logger_name is None else logger_name
+    path_to_logs = Path("logs") if path is None else Path(path)
     path_to_logs.mkdir(parents=True, exist_ok=True)
-    filename = path_to_logs / f'{logger_name}.log'
-    print(f'Log file path: {filename}')
-    logging.basicConfig(level=level,
-                        format=u'%(levelname)-8s [%(asctime)s] %(message)s',
-                        filename=filename)
-    logger = logging.getLogger(logger_name)
+    filename = path_to_logs / f"{logger_name}.log"
+    print(f'Log file path: {filename.absolute()}')
+
+    # create formatter with level name, module, line number, time and message
+    formatter = logging.Formatter(
+        "%(levelname)-8s [%(asctime)s] %(name)s:%(lineno)d: %(message)s"
+    )
+
+    # create file handler
+    file_handler = logging.FileHandler(filename)
+    file_handler.setLevel(level)
+    file_handler.setFormatter(formatter)
+
+    # create logger
+    logger = logging.getLogger(name=logger_name)
+    logger.addHandler(file_handler)
+    logger.setLevel(level)
+    if add_stdout:
+        # create stdout handler
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(level)
+        stdout_handler.setFormatter(formatter)
+        logger.addHandler(stdout_handler)
     return logger
 
 
